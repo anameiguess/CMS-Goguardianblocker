b9d4c2d3c00cffbf4bee639757bf846ff6360340
anameiguess <99776442+anameiguess@users.noreply.github.com>
2022-05-11 17:30:03 -0500

    Add files via upload

---
+++ Bunker v4.4/config.js
@@ -0,0 +1,16 @@
+window._config = {
+	"apps": [
+		"yt-cl",
+		"comms",
+		"prxyz",
+		"info",
+		"mpviewer",
+		"conn_stats",
+		"hacker-news-tile",
+		"sample-games",
+		"count-down",
+		"schedule",
+		"tutorial",
+		"wordle"
+	]
+}
